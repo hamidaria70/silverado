69a8b82d4dc32e51578b12a60adb68ade03519f0
Hamid Aria <hamidaria.70@gmail.com>
2022-01-23 00:52:02 +0330

    Adding read from input

--- booking-app/main.go
+++ booking-app/main.go
@@ -6,12 +6,22 @@ import (
 
 func main() {
 
-	var conferenceName string = "Go Conference"
 	const conferenceTickets int = 50
+	var conferenceName string = "Go Conference"
 	var remainingTickets int = 50
+	var userName string
+	var userTickets int
 
 	fmt.Printf("Welcome to our %v Booking App\n", conferenceName)
 	fmt.Printf("Availabe tickets are %v and Remaining Tickets are %v\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+	fmt.Println("Please enter your name:")
 
+	fmt.Scan(&userName)
+	fmt.Println("Please enter your tickets:")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("User %v booked %v tickets and remaining tickets are %v\n", userName, userTickets, remainingTickets)
 }
