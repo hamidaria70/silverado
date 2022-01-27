0f02a8cddc5b928c055979b67cc84af9bbcf9303
Hamid Aria <hamidaria.70@gmail.com>
2022-01-27 20:11:54 +0330

    Adding first array

--- booking-app/main.go
+++ booking-app/main.go
@@ -10,18 +10,25 @@ func main() {
 	var conferenceName string = "Go Conference"
 	var remainingTickets int = 50
 	var userName string
+	var lastName string
 	var userTickets int
 
 	fmt.Printf("Welcome to our %v Booking App\n", conferenceName)
 	fmt.Printf("Availabe tickets are %v and Remaining Tickets are %v\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 	fmt.Println("Please enter your name:")
-
 	fmt.Scan(&userName)
+	fmt.Println("Please enter your last name:")
+	fmt.Scan(&lastName)
 	fmt.Println("Please enter your tickets:")
 	fmt.Scan(&userTickets)
 
+	var bookings [50]string
+	bookings[0] = userName + " " + lastName
+
+	fmt.Printf("first element of array is %v\n", bookings[0])
+	fmt.Printf("the whole array is %v\n", bookings)
 	remainingTickets = remainingTickets - userTickets
 
-	fmt.Printf("User %v booked %v tickets and remaining tickets are %v\n", userName, userTickets, remainingTickets)
+	fmt.Printf("User %v %v booked %v tickets and remaining tickets are %v\n", userName, lastName, userTickets, remainingTickets)
 }
