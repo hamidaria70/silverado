843afa0aad046f515eb80847ed117e64fab4d3b6
Hamid Aria <hamidaria.70@gmail.com>
2022-01-29 12:52:12 +0330

    Adding input validation

--- booking-app/main.go
+++ booking-app/main.go
@@ -20,19 +20,25 @@ func main() {
 		var userName string
 		var lastName string
 		var userTickets int
+		var email string
 
 		fmt.Println("Please enter your name:")
 		fmt.Scan(&userName)
 		fmt.Println("Please enter your last name:")
 		fmt.Scan(&lastName)
+		fmt.Println("Please enter your email address:")
+		fmt.Scan(&email)
 		fmt.Println("Please enter your tickets:")
 		fmt.Scan(&userTickets)
 
-		bookings = append(bookings, userName+" "+lastName)
+		var isValidName bool = len(userName) > 2 && len(lastName) > 2
+		var isValidEmail bool = strings.Contains(email,"@")
+		var isValidTicketNumber bool = userTickets > 0 && userTickets <= remainingTickets
 
-		if userTickets <= remainingTickets{
+		if isValidEmail && isValidName && isValidTicketNumber {
 			remainingTickets = remainingTickets - userTickets
-			
+			bookings = append(bookings, userName+" "+lastName)
+
 			fmt.Printf("User %v %v booked %v tickets and remaining tickets are %v\n", userName, lastName, userTickets, remainingTickets)
 
 			var firstNames []string
@@ -47,8 +53,15 @@ func main() {
 				break
 			}
 		} else {
-			fmt.Printf("%v is more than available(%v) tickets\n",userTickets, remainingTickets)
+			if !isValidName {
+				fmt.Println("First or Last name in too short")
+			}
+			if !isValidEmail {
+				fmt.Println("Your email does not exist")
+			}
+			if !isValidTicketNumber {
+				fmt.Println("number of tickets is not valid")
+			}
 		}
-		
 	}
 }
