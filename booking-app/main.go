a31c98e80f3b968b8a381d05f056095cbb50dd1e
Hamid Aria <hamidaria.70@gmail.com>
2022-01-30 11:29:50 +0330

    Adding helper file for isValidInput function

--- booking-app/main.go
+++ booking-app/main.go
@@ -59,14 +59,6 @@ func getFirstNames(bookings []string) []string{
 	return firstNames
 }
 
-func inputValidation(userName string,lastName string,email string,userTickets uint) (bool, bool, bool) {
-	var isValidName bool = len(userName) > 2 && len(lastName) > 2
-	var isValidEmail bool = strings.Contains(email,"@")
-	var isValidTicketNumber bool = userTickets > 0 && userTickets <= remainingTickets
-
-	return isValidEmail, isValidName, isValidTicketNumber
-}
-
 func getUserInfo() (string, string, uint, string) {
 	var userName string
 	var lastName string
