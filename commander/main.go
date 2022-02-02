dc6b88e2013939c050d63278b007795afa48d630
Hamid Aria <hamidaria.70@gmail.com>
2022-02-02 16:43:35 +0330

    Adding code to get access to the cluster

--- commander/main.go
+++ commander/main.go
@@ -1,32 +1,61 @@
 package main
 
 import (
-	"os"
+//	"os"
 	"fmt"
-	"io/ioutil"
+	"log"
+	"context"
+//	"io/ioutil"
 	"path/filepath"
+	"k8s.io/client-go"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/util/homedir"
+	"k8s.io/client-go/tools/clientcmd"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func main() {
-	kubeConfig := getConfig()
-	fmt.Printf("Path to the kubeConfig is %v.\n", kubeConfig)
-	kubeConfigData := showConfig(kubeConfig)
-	fmt.Println(kubeConfigData)
-
-}
-
-func getConfig() string {
-	kubeConfig := filepath.Join(
-		os.Getenv("HOME"), ".kube", "config",
+//	kubeConfig := getConfig()
+//	fmt.Printf("Path to the kubeConfig is %v.\n", kubeConfig)
+//	kubeConfigData := showConfig(kubeConfig)
+//	fmt.Println(kubeConfigData)
+	kubeConfig, err := clientcmd.BuildConfigFromFlags(
+		"", filepath.Join(homedir.HomeDir(), ".kube", "config"),
 	)
-	return kubeConfig
+	checkErr(err)
+
+	k8s, err := kubernetes.NewForConfig(kubeConfig)
+	checkErr(err)
+
+	nodeList, err := k8s.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
+	checkErr(err)
+
+	for _,nameSpace := range nodeList.Items {
+		fmt.Println(nameSpace.Name)
+	}
+	podList , err := clientset.CoreV1().pods("").List(context.Background(),metav1.ListOptions{})
+	checkErr(err)
 }
 
-func showConfig(kubeConfig string) string {
-    data, err := ioutil.ReadFile(kubeConfig)
-
-    if err != nil {
-        fmt.Println("File reading error", err)
-    }
-    return string(data)
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
 }
+
+
+//func getConfig() string {
+	//kubeConfig := filepath.Join(
+		//os.Getenv("HOME"), ".kube", "config",
+	//)
+	//return kubeConfig
+//}
+
+//func showConfig(kubeConfig string) string {
+    //data, err := ioutil.ReadFile(kubeConfig)
+
+    //if err != nil {
+        //fmt.Println("File reading error", err)
+    //}
+    //return string(data)
+//}
