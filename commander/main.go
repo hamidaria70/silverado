f8ef3c5764a7e915f27ece050834d72d1c4a9989
Hamid Aria <hamidaria.70@gmail.com>
2022-02-02 15:19:54 +0330

    Getting content and path of kubeConfig

--- commander/main.go
+++ commander/main.go
@@ -8,14 +8,25 @@ import (
 )
 
 func main() {
+	kubeConfig := getConfig()
+	fmt.Printf("Path to the kubeConfig is %v.\n", kubeConfig)
+	kubeConfigData := showConfig(kubeConfig)
+	fmt.Println(kubeConfigData)
+
+}
+
+func getConfig() string {
 	kubeConfig := filepath.Join(
 		os.Getenv("HOME"), ".kube", "config",
 	)
+	return kubeConfig
+}
 
+func showConfig(kubeConfig string) string {
     data, err := ioutil.ReadFile(kubeConfig)
+
     if err != nil {
         fmt.Println("File reading error", err)
-        return
     }
-    fmt.Println("Contents of file:", string(data))
+    return string(data)
 }
