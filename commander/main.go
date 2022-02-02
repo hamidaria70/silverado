c67d427b35fc63b6a9f89e78b348788b404bbeb8
Hamid Aria <hamidaria.70@gmail.com>
2022-02-02 22:46:35 +0330

    Removing extra lines

--- commander/main.go
+++ commander/main.go
@@ -1,24 +1,25 @@
 package main
 
 import (
-//	"os"
+	//	"os"
+	"context"
 	"fmt"
 	"log"
-	"context"
-//	"io/ioutil"
+
+	//	"io/ioutil"
 	"path/filepath"
-	"k8s.io/client-go"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/util/homedir"
-	"k8s.io/client-go/tools/clientcmd"
+	//	"k8s.io/client-go"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
 )
 
 func main() {
-//	kubeConfig := getConfig()
-//	fmt.Printf("Path to the kubeConfig is %v.\n", kubeConfig)
-//	kubeConfigData := showConfig(kubeConfig)
-//	fmt.Println(kubeConfigData)
+	//	kubeConfig := getConfig()
+	//	fmt.Printf("Path to the kubeConfig is %v.\n", kubeConfig)
+	//	kubeConfigData := showConfig(kubeConfig)
+	//	fmt.Println(kubeConfigData)
 	kubeConfig, err := clientcmd.BuildConfigFromFlags(
 		"", filepath.Join(homedir.HomeDir(), ".kube", "config"),
 	)
@@ -30,11 +31,11 @@ func main() {
 	nodeList, err := k8s.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{})
 	checkErr(err)
 
-	for _,nameSpace := range nodeList.Items {
+	for _, nameSpace := range nodeList.Items {
 		fmt.Println(nameSpace.Name)
 	}
-	podList , err := clientset.CoreV1().pods("").List(context.Background(),metav1.ListOptions{})
-	checkErr(err)
+	//	podList , err := clientset.CoreV1().pods("").List(context.Background(),metav1.ListOptions{})
+	//	checkErr(err)
 }
 
 func checkErr(err error) {
@@ -43,19 +44,18 @@ func checkErr(err error) {
 	}
 }
 
-
 //func getConfig() string {
-	//kubeConfig := filepath.Join(
-		//os.Getenv("HOME"), ".kube", "config",
-	//)
-	//return kubeConfig
+//kubeConfig := filepath.Join(
+//os.Getenv("HOME"), ".kube", "config",
+//)
+//return kubeConfig
 //}
 
 //func showConfig(kubeConfig string) string {
-    //data, err := ioutil.ReadFile(kubeConfig)
+//data, err := ioutil.ReadFile(kubeConfig)
 
-    //if err != nil {
-        //fmt.Println("File reading error", err)
-    //}
-    //return string(data)
+//if err != nil {
+//fmt.Println("File reading error", err)
+//}
+//return string(data)
 //}
