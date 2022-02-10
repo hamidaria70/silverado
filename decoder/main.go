07661d067fc883db81795f33e6e8b75becb13f64
Hamid Aria <hamidaria.70@gmail.com>
2022-02-11 00:18:56 +0330

    Getting all value in test key from redis

--- decoder/main.go
+++ decoder/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	claims := jwt.MapClaims{}
+	tokenSlice := []string{}
 
 	fmt.Println("Go Redis Tutorial")
 
@@ -20,15 +21,21 @@ func main() {
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
 
-	val, err := client.LRange("test", -1, -1).Result()
+	val, err := client.LRange("test", 0, -1).Result()
 	if err != nil {
 		fmt.Println(err)
 	}
-	tokenString := strings.Trim(strings.TrimSpace(strings.Split(val[0], "Bearer")[1]), "\"}")
 
-	jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
-		return []byte(tokenString), nil
-	})
+	fmt.Printf("The length of val slice is %d.\n", len(val))
+	for _, element := range val {
+		tokenSlice = append(tokenSlice, strings.Trim(strings.TrimSpace(strings.Split(element, "Bearer")[1]), "\"}"))
+	}
+
+	for _, token := range tokenSlice {
+		jwt.ParseWithClaims(token, claims, func(token *jwt.Token) (interface{}, error) {
+			return []byte(token), nil
+		})
+	}
 
 	for key, val := range claims {
 		fmt.Printf("Key: %v , value: %v\n", key, val)
