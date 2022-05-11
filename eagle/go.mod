2945a313785cb57c2e351a2d03e80212374c6668
Hamid Aria <hamidaria.70@gmail.com>
2022-05-11 10:58:18 +0430

    Updating code by getting memory and cpu usage

--- eagle/go.mod
+++ eagle/go.mod
@@ -1,3 +1,10 @@
 module eagle
 
 go 1.17
+
+require (
+	code.cloudfoundry.org/bytefmt v0.0.0-20211005130812-5bb3c17173e5
+	github.com/mackerelio/go-osstat v0.2.2
+)
+
+require golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
--- eagle/main.go
+++ eagle/main.go
@@ -8,6 +8,11 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"time"
+
+	"code.cloudfoundry.org/bytefmt"
+	"github.com/mackerelio/go-osstat/cpu"
+	"github.com/mackerelio/go-osstat/memory"
 )
 
 func checkError(err error) {
@@ -16,22 +21,59 @@ func checkError(err error) {
 	}
 }
 
-func main() {
+func sshConnect() {
+	cmd := exec.Command("ssh", "office", "echo 'blah...blah....blah' > golang.txt")
+	var out bytes.Buffer
+	cmd.Stdout = &out
 
+	err := cmd.Run()
+	checkError(err)
+
+}
+
+func readSshConfig() {
 	home, _ := os.UserHomeDir()
 	sshConfig := (filepath.Join(home, ".ssh", "config"))
 	file, err := ioutil.ReadFile(sshConfig)
 
 	checkError(err)
 	fmt.Println(string(file))
+}
 
-	cmd := exec.Command("ssh", "office", "echo 'blah...blah....blah' > golang.txt")
-	var out bytes.Buffer
-	cmd.Stdout = &out
-
-	err = cmd.Run()
+func memoryUsage() {
+	memory, err := memory.Get()
 	if err != nil {
-		log.Fatal(err)
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		return
 	}
 
+	fmt.Printf("memory total: %v\n", bytefmt.ByteSize(memory.Total))
+	fmt.Printf("memory used: %v bytes\n", bytefmt.ByteSize(memory.Used))
+	fmt.Printf("memory cached: %v bytes\n", bytefmt.ByteSize(memory.Cached))
+	fmt.Printf("memory free: %v bytes\n", bytefmt.ByteSize(memory.Free))
+}
+
+func cpuUsage() {
+	before, err := cpu.Get()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		return
+	}
+	time.Sleep(time.Duration(1) * time.Second)
+	after, err := cpu.Get()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		return
+	}
+	total := float64(after.Total - before.Total)
+	fmt.Printf("cpu user: %f %%\n", float64(after.User-before.User)/total*100)
+	fmt.Printf("cpu system: %f %%\n", float64(after.System-before.System)/total*100)
+	fmt.Printf("cpu idle: %f %%\n", float64(after.Idle-before.Idle)/total*100)
+}
+
+func main() {
+	readSshConfig()
+	sshConnect()
+	memoryUsage()
+	cpuUsage()
 }
